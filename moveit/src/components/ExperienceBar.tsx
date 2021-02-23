c4adfbbd20b8883feb7bcc4869e03b6c3c1181cc
Fernando Dutra Neres <fernandodutraneres@gmail.com>
2021-02-22 22:41:24 -0300

    Controlling state and grow up progress bar on setInterval function

--- moveit/src/components/ExperienceBar.tsx
+++ moveit/src/components/ExperienceBar.tsx
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react';
+
 export default function ExperienceBar() {
+  const maxProgress = 600;
+  const [level, setLevel] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const getPercent = function(p: number) {
+    const percent = (p * 100) / maxProgress;
+    return Math.ceil(percent);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (progress < maxProgress) {
+        setProgress(progress + 1);
+      } else {
+        setLevel(level + 1);
+        setProgress(0);
+      }
+    }, 100);
+    
+    return () => {
+      clearInterval(timer);
+    }
+  }, [progress, level]);
+
   return (
-    <header className="experience-bar">
-      <span>0 exp</span>
-      <div>
-        <div style={{ width: '60%' }} />
-        <span className="corrent-experience" style={{ left: '60%' }}>400 exp</span>
-      </div>
-      <span>600 exp</span>
-    </header>
+    <div>
+      <header className="experience-bar">
+        <span>0 exp</span>
+        <div>
+          <div style={{ width: `${getPercent(progress)}%` }} />
+          <span className="corrent-experience" style={{ left: `${getPercent(progress)}%` }}>{progress} exp</span>
+        </div>
+        <span>{maxProgress} exp</span>
+      </header>
+    </div>
   )
 }
